7ad67948b1ad7d710eca2fead3ae9bfd1e3944d4
stolnikov-ea <stolnikov@protei.ru>
2026-09-16 09:57:59 +0300

    Fixed allure and forward geocode tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import logging
 import pytest
 import allure
+import os
 from datetime import datetime
 
 from config.settings import USER_NAME, LOG_LEVEL, LOG_FORMAT
@@ -14,12 +15,15 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 def pytest_configure(config):
+
+    allure_dir = config.getoption("--alluredir")
+    env_file = os.path.join(allure_dir, "environment.properties")
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    if hasattr(config, "allure_properties"):
-        config.allure_properties.append(("Дата запуска", now))
-        config.allure_properties.append(("Пользователь", USER_NAME))
-        config.allure_properties.append(("Проект", "geocodeDDT"))
+    with open (env_file, "a") as f:
+        f.write(f"Дата_запуска: {now}\n")
+        f.write(f"Пользователь: {USER_NAME}\n")
+        f.write(f"Проект: geocodeDDT\n")
 
 @pytest.fixture(scope = "session")
 def client():
